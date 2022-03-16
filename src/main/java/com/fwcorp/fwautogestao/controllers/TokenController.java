018a396a56f3b5bfd5c74358e95c99779a406d6c
wnra <wilson.almeida@gft.com>
2022-03-16 20:04:18 -0300

    criação de endpoint pra verificar token

--- src/main/java/com/fwcorp/fwautogestao/controllers/TokenController.java
+++ src/main/java/com/fwcorp/fwautogestao/controllers/TokenController.java
@@ -1,14 +1,20 @@
 package com.fwcorp.fwautogestao.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fwcorp.fwautogestao.entities.Cargo;
+import com.fwcorp.fwautogestao.entities.RespostaServidor;
 import com.fwcorp.fwautogestao.entities.TokenRegistro;
 import com.fwcorp.fwautogestao.services.TokenRegistroService;
+import com.fwcorp.fwautogestao.util.GeradorRespostaServidor;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,6 +38,12 @@ public class TokenController {
 	public ResponseEntity<?> gerarTokenDeRegistroParaOperador() {
 		return ResponseEntity.ok(
 				tokenService.salvarToken(new TokenRegistro(new Cargo("OPERADOR"))));
+	}
+	
+	@GetMapping("verificar/{token}")
+	public ResponseEntity<RespostaServidor> verificarToken(@PathVariable String token){
+		TokenRegistro tokenObtido = tokenService.buscarToken(token);
+		return GeradorRespostaServidor.gerarRespostaServidorStatusOk(tokenObtido.getCargo().getNome());
 	}
 
 }
