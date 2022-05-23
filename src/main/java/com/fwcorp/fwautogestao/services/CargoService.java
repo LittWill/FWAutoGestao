4bab0fd123115af2e6534f17e77e13ed2755936d
wnra <wilson.almeida@gft.com>
2022-05-23 11:16:23 -0300

    Lombok subs

--- src/main/java/com/fwcorp/fwautogestao/services/CargoService.java
+++ src/main/java/com/fwcorp/fwautogestao/services/CargoService.java
@@ -2,18 +2,16 @@ package com.fwcorp.fwautogestao.services;
 
 import com.fwcorp.fwautogestao.entities.Cargo;
 import com.fwcorp.fwautogestao.repositories.CargoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class CargoService {
 
     private final CargoRepository cargoRepository;
-
-    public CargoService(CargoRepository cargoRepository) {
-        this.cargoRepository = cargoRepository;
-    }
 
     public Cargo obterCargo(String nome){
         return this.cargoRepository.findById(nome).orElseThrow(() -> new RuntimeException("Cargo não encontrado!"));
